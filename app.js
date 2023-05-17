55714649224eadaaacab39b62e91ba84b9f1f3c8
unknown <ladexlad2@gmail.com>
2023-05-17 04:30:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const app = express();
+// const jwt = require("jsonWebtoken");
+
+app.use(express.json());
+
+const users = [];
+
+const getProject = (req, res) => {
+  res.json(users);
+};
+const user = async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).json({ msg: `welcome ${user.name}` });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+const login = async (req, res) => {
+  const user = users.find((user) => (user.name = req.body.name));
+  if (user == null) {
+    res.status(400).send("cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("success");
+    } else {
+      res.send("user not allowed");
+    }
+  } catch (error) {
+    res.status(500).send();
+  }
+};
+
+app.get("/api/project/", getProject);
+app.post("/api/user", user);
+app.post("/api/login", login);
+
+app.listen(3000, (req, res) => {
+  console.log("listening on port 3000");
+});
